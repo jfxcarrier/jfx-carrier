4f99789103859a55a795b0b3aeddbfce996c822c
jfxcarrier <jfxcarrier@gmail.com>
2026-04-28 23:51:04 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDDOySGXJlcgkE35oCzDmcOSH9FW2E84Xw",
+  authDomain: "jfx-carrier.firebaseapp.com",
+  projectId: "jfx-carrier",
+  storageBucket: "jfx-carrier.firebasestorage.app",
+  messagingSenderId: "414857563650",
+  appId: "1:414857563650:web:e613dfc0c53d620550211b",
+  measurementId: "G-33R15F10JB"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
